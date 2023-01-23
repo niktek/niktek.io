c1f2f9c467b89bc6642fb9893f625d42d5f7db9f
Nik <nik@niktek.io>
2023-01-23 13:49:17 +1100

    Removed public

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import { VERCEL_ENV } from '$env/static/public';
 	export let data;
 </script>
 <p>in feature branch preview mode</p>
 <p>{data.vercelEnv } <br /> {data.vercelUrl}</p>
-<p>{VERCEL_ENV}</p>
