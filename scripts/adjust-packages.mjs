ef3ab1a6d16668e5d24f01e86f6e19a85622fdc5
Nik <nik@niktek.io>
2023-05-19 12:27:18 +1000

    Attempt deployment adjustments

---
+++ scripts/adjust-packages.mjs
@@ -0,0 +1,17 @@
+import isCI from 'is-ci';
+import {readFileSync, writeFileSync} from 'fs';
+
+let pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+if (isCI) {
+    console.log('CI detected, inserting devDependencies from deployConfig');
+    for ([devDep, version] of Object.entries(pkg.deployConfig.devDependencies)) {
+        pkg.devDependencies[devDep] = version;
+    }
+}else{
+    console.log('No CI detected, setting @skeletonlabs/skeleton to workspace:*');
+    for ([devDep, version] of Object.entries(pkg.deployConfig.devDependencies)) {
+        pkg.devDependencies[devDep] = 'workspace:*';
+    }
+}
+writeFileSync('./package.json', JSON.stringify(pkg, null, 2), 'utf8');
+process.exit(0);
