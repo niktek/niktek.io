bf8f86678c74525e176c92e57760e64fadc357f1
Nik <nik@niktek.io>
2023-05-19 12:43:52 +1000

    Remove infinite loop

--- scripts/adjust-packages.mjs
+++ scripts/adjust-packages.mjs
@@ -1,17 +1,25 @@
 import isCI from 'is-ci';
-import {readFileSync, writeFileSync} from 'fs';
+import {readFileSync, writeFileSync, execSync} from 'fs';
 
 let pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+let clean = true;
+
 if (isCI) {
     console.log('CI detected, inserting devDependencies from deployConfig');
     for (const [devDep, version] of Object.entries(pkg.deployConfig.devDependencies)) {
-        pkg.devDependencies[devDep] = version;
+        if (pkg.devDependencies[devDep] !== version) {
+            pkg.devDependencies[devDep] = version;
+            clean = false;
+        }
     }
-}else{
+} else {
     console.log('No CI detected, setting @skeletonlabs/skeleton to workspace:*');
     for (const [devDep, version] of Object.entries(pkg.deployConfig.devDependencies)) {
         pkg.devDependencies[devDep] = 'workspace:*';
     }
 }
 writeFileSync('./package.json', JSON.stringify(pkg, null, 2), 'utf8');
+if (clean === false) {
+    execSync('pnpm install --no-frozen-lockfile')
+}
 process.exit(0);
